af4de4b28726200575bc2c4d600613c04d8fb902
Francisco Roa Prieto <franciscoroaprieto@gmail.com>
2024-01-31 16:58:50 +0100

    another change

--- terraform/example.tf
+++ terraform/example.tf
@@ -30,5 +30,7 @@ resource "github_membership" "membership_for_user_x" {
   username = "user_x"
 }
 
+# cnahge
+
 # Add a user to the organization
 # resource "github_membership" "membership_for_user_x" { }
